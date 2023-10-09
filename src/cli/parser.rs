ef0d8dbc70c273aa2f1792c7a04815f2dcc3ab4b
James ABIB <james.bidule@gmail.com>
2023-10-09 11:56:46 +0200

    chore: Update CLI argument parsing and error handling
    - Updated the short and long flags for the 'operation' argument in the CliArgs struct.
    - Changed the default value for the 'repository' argument in the CliArgs struct.
    - Added required and default values for the 'directory' argument in the CliArgs struct.
    - Updated the short flag for the 'source' argument in the CliArgs struct.
    - Added error handling to check if '--source' or '-s' is provided when operation is 'U'.
    - Improved error message when '--source' or '-s' is missing during an upload operation.

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -2,20 +2,32 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct CliArgs {
-    #[clap(short, long, required = true)]
+    #[clap(
+        short = 'o',
+        long = "operation",
+        required = true
+    )]
     pub operation: String,
 
     #[clap(
         short = 'r',
         long = "repository",
         required = true,
-        default_value = "depot-local"
+        default_value = "depot-local-snapshot"
     )]
     pub repository: Option<String>,
 
-    #[clap(short = 'd', long = "directory")]
+    #[clap(
+        short = 'd',
+        long = "directory",
+        required = true,
+        default_value = "rp/omer/ihm/homere-DV05.zip"
+    )]
     pub directory: Option<String>,
 
-    #[clap(short = 's', long = "source")]
+    #[clap(
+        short = 's',
+        long = "source"
+    )]
     pub source: Option<String>,
 }
